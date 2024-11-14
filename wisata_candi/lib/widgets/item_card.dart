4b26db69bf04d257ad719a5b3f74fef1ed2ad2d5
Veskebte <nathasyapernidyaashari_2327250063@mhs.mdp.ac.id>
2024-11-14 12:27:39 +0700

    Make home_screen.dart
    Pertemuan 19

---
+++ wisata_candi/lib/widgets/item_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
+
+class ItemCard extends StatelessWidget {
+  // TODO: 1. Deklarasikan variabel yang dibutuhkan dan pasang pada konstruktor
+  const ItemCard({super.key, required this.candi});
+  final Candi candi;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: 6. Implementasi routing ke DetailScreen
+    return Card(
+      // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari Cari
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      margin: EdgeInsets.all(4.0),
+      elevation: 1,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // TODO: 3. Buat Image sebagai anak dari Column
+          Expanded(
+            // TODO: 7. Implementasi Hero animation
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.asset(
+                candi.imageAsset,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // TODO: 4.0. Buat Text sebagai anak dari Column
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 8.0),
+            child: Text(
+              candi.name,
+              style:
+                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          // TODO: 5. Buat Text sebagai anak dari Column
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, bottom: 8.0),
+            child: Text(
+              candi.type,
+              style: const TextStyle(fontSize: 12.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
